d29ab1efd9d852037a6a1a8c8a5a6964f3d3df5e
Savvas Dalkitsis <savvas.dalkitsis@gmail.com>
2016-05-09 23:35:50 +0100

    Refactoring

--- app/src/main/java/com/savvasdalkitsis/mondo/repository/PreferencesCredentialsRepository.java
+++ app/src/main/java/com/savvasdalkitsis/mondo/repository/PreferencesCredentialsRepository.java
@@ -1,7 +1,10 @@
 package com.savvasdalkitsis.mondo.repository;
 
 import com.savvasdalkitsis.mondo.infra.preferences.MondoPreferences;
-import com.savvasdalkitsis.mondo.infra.preferences.PreferenceKeys;
+
+import static com.savvasdalkitsis.mondo.infra.preferences.PreferenceKeys.KEY_ACCESS_TOKEN;
+import static com.savvasdalkitsis.mondo.infra.preferences.PreferenceKeys.KEY_ACCOUNT_ID;
+import static com.savvasdalkitsis.mondo.infra.preferences.PreferenceKeys.KEY_REFRESH_TOKEN;
 
 public class PreferencesCredentialsRepository implements CredentialsRepository {
 
@@ -13,31 +16,39 @@ public class PreferencesCredentialsRepository implements CredentialsRepository {
 
     @Override
     public void saveAccessToken(String accessToken) {
-        mondoPreferences.putStringPreference(PreferenceKeys.KEY_ACCESS_TOKEN, accessToken);
+        savePref(accessToken, KEY_ACCESS_TOKEN);
     }
 
     @Override
     public void saveRefreshToken(String refreshToken) {
-        mondoPreferences.putStringPreference(PreferenceKeys.KEY_REFRESH_TOKEN, refreshToken);
+        savePref(refreshToken, KEY_REFRESH_TOKEN);
     }
 
     @Override
     public void saveAccountId(String accountId) {
-        mondoPreferences.putStringPreference(PreferenceKeys.KEY_ACCOUNT_ID, accountId);
+        savePref(accountId, KEY_ACCOUNT_ID);
     }
 
     @Override
     public String getAccessToken() {
-        return mondoPreferences.getStringPreference(PreferenceKeys.KEY_ACCESS_TOKEN);
+        return pref(KEY_ACCESS_TOKEN);
     }
 
     @Override
     public String getRefreshToken() {
-        return mondoPreferences.getStringPreference(PreferenceKeys.KEY_REFRESH_TOKEN);
+        return pref(KEY_REFRESH_TOKEN);
     }
 
     @Override
     public String getAccountId() {
-        return mondoPreferences.getStringPreference(PreferenceKeys.KEY_ACCOUNT_ID);
+        return pref(KEY_ACCOUNT_ID);
+    }
+
+    private void savePref(String accountId, String keyAccountId) {
+        mondoPreferences.putStringPreference(keyAccountId, accountId);
+    }
+
+    private String pref(String keyAccountId) {
+        return mondoPreferences.getStringPreference(keyAccountId);
     }
 }
